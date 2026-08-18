10a5e1ce3269c3350418361ea2a23e56d6a00938
Pranav1065 <maxpranav2009@gmail.com>
2026-08-18 10:40:54 +0530

    Create Question2.cpp

---
+++ OOP/Experiment 8/Question2.cpp
@@ -0,0 +1,36 @@
+//store details of five customers
+#include <iostream>
+using namespace std;
+class Customer
+{
+    int id;
+    string name;
+public:
+    void getData()
+    {
+        cout << "Enter ID: ";
+        cin >> id;
+        cout << "Enter Name: ";
+        cin >> name;
+    }
+    void display()
+    {
+        cout << "ID: " << id << "  Name: " << name << endl;
+    }
+};
+int main()
+{
+    Customer c[5];
+    int i;
+    for(i = 0; i < 5; i++)
+    {
+        cout << "\nCustomer " << i + 1 << endl;
+        c[i].getData();
+    }
+    cout << "\nCustomer Details:\n";
+    for(i = 0; i < 5; i++)
+    {
+        c[i].display();
+    }
+    return 0;
+}
