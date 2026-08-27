16ec3ded4a9a4066d058f57974399f8c48563370
Pranav1065 <maxpranav2009@gmail.com>
2026-08-27 10:48:53 +0530

    Create Question1.cpp

---
+++ OOP/Practical 8/Question1.cpp
@@ -0,0 +1,28 @@
+//WAP to define a class "SI" having data members as Principal, Duration, ROI. declare ROI as static member variable. Calculate SI and display it.
+#include <iostream>
+using namespace std;
+class SI{
+    static float ROI;
+    unsigned long prin;
+    float dur;
+    public:
+    void acc(){
+        cout << "Enter principal amount ₹";
+        cin >> prin;
+        cout << "Enter duration (in years): ";
+        cin >> dur;
+    }
+    void dis(){
+        cout << "Interest is: " << (prin * ROI * dur) / 100 << endl;
+        cout << "Amount: ₹" << prin + (prin * ROI * dur)/100 << endl;
+    }
+};
+
+float SI::ROI = 6.0;
+int main() {
+    SI ds;
+    ds.acc();
+    ds.dis();
+
+    return 0;
+}
